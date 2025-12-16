5b231ba037b71e856b7d59ec67c324c4ca822cda
rakshasubramanya08 <rakshasubramanya08@gmail.com>
2025-12-16 12:42:04 +0100

    added usestate hooks

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 
-export default function TabIndex() {
-
+export default function App() {
+  const [display, setDisplay] = useState("0");
+  const [firstValue, setFirstValue] = useState<string | null>(null);
+  const [operator, setOperator] = useState<string | null>(null);
 }
